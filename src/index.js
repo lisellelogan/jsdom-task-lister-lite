56af058c9a52421dad42d8ea0b73f3e476023082
lisellelogan <liselle.logan@yahoo.com>
2021-03-23 20:09:37 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
+  // select form
+  const taskForm = document.getElementById('create-task-form')
+  // select container to put task
+  const listTask = document.getElementById('tasks')
+
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+    // listen for submit 
+  taskForm.addEventListener("submit", handleSubmit);
 });
+
+// preventDefault() in order to update DOM
+function handleSubmit(event) {
+  event.preventDefault()
+  const newTask = document.getElementById('new-task-description').value
+  listTask.innerHTML += `<li>${newTask}</li>`
+}
